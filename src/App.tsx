7e861e1d1a6b54af698d791ba2a187718d95cc75
Diogo Galdino Stihler <diogo.galdino@philips.com>
2022-05-05 23:30:19 -0300

    [REACTJS] - defaut structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home/Home";
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>} ></Route>
+      </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
